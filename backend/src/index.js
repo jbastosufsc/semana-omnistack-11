973d912d0a67f41479a33e2693986770d4cadfce
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-03-29 16:19:28 -0300

    Install cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+import cors = require('cors');
 const app = express();
 const port = 3000;
 const routes = require('./routes');
 
+//for development enviroment
+app.use(cors());
+
+/*for production enviroment
+ app.use(cors({
+ origin: 'http://linkdaaplicação.com'
+}))
+*/
 app.use(express.json());
 app.use(routes);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
