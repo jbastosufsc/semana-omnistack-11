69b16692fbd63393d1afb11224fad07836be5ecc
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-03-31 11:51:03 -0300

    Set up axios and register ongs

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,34 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import api from '../../services/api';
+
 export default function Register() {
+  const [ name, setName ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ whatsapp, setWhatsapp ] = useState('');
+  const [ city, setCity ] = useState('');
+  const [ uf, setUf ] = useState('');
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    };
+
+    try {
+      const response = await api.post('ongs', data);
+      alert(`Seu ID de acesso: ${response.data.id}`);
+    } catch (error) {
+      alert('Erro no cadastro. Tente novamente.');
+    }
+  }
+
   return (
-    <div className='register-container'>
-      <div className='content'>
+    <div className="register-container">
+      <div className="content">
         <section>
-          <img src={logoImg} alt='Be the Hero' />
+          <img src={logoImg} alt="Be the Hero" />
           <h1>Cadastro</h1>
           <p>
-            Faça seu cadastro, entre na plataforma e ajude as pessoas a
-            encontrarem os casos da sua ONG.
+            Faça seu cadastro, entre na plataforma e ajude as pessoas a encontrarem os casos da sua
+            ONG.
           </p>
-          <Link className='back-link' to='/'>
-            <FiArrowLeft size={16} color='#e02041' />
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#e02041" />
             Não tenho cadastro
           </Link>
         </section>
-        <form action=''>
-          <input type='text' placeholder='Nome da ONG' />
-          <input type='text' placeholder='E-mail' />
-          <input type='text' placeholder='WhatsApp' />
-          <div className='input-group'>
-            <input type='text' placeholder='Cidade' />
-            <input type='text' placeholder='UF' style={{ width: 80 }} />
+        <form onSubmit={handleRegister} action="">
+          <input
+            type="text"
+            placeholder="Nome da ONG"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="WhatsApp"
+            value={whatsapp}
+            onChange={(e) => setWhatsapp(e.target.value)}
+          />
+          <div className="input-group">
+            <input
+              type="text"
+              placeholder="Cidade"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="UF"
+              value={uf}
+              onChange={(e) => setUf(e.target.value)}
+              style={{ width: 80 }}
+            />
           </div>
-          <button className='button' type='submit'>
+          <button className="button" type="submit">
             {' '}
             Cadastrar
           </button>
