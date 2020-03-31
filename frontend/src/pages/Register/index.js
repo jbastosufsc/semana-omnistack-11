5cedc7f7f708576d4bf26fe3ba552bab05b24da4
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-03-31 11:53:42 -0300

    Push user to login page after register ong

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -12,6 +12,8 @@ export default function Register() {
   const [ whatsapp, setWhatsapp ] = useState('');
   const [ city, setCity ] = useState('');
   const [ uf, setUf ] = useState('');
+
+  const history = useHistory();
 
   async function handleRegister(e) {
     e.preventDefault();
@@ -27,6 +29,7 @@ export default function Register() {
     try {
       const response = await api.post('ongs', data);
       alert(`Seu ID de acesso: ${response.data.id}`);
+      history.push('/');
     } catch (error) {
       alert('Erro no cadastro. Tente novamente.');
     }
