d3aee95ac3995166f870520ca2e741bc48c2226e
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-04-02 14:58:12 -0300

    Finish mobile app

--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -1,63 +1,79 @@
-import { StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import { StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
+
 export default StyleSheet.create({
-  container: {
+  incidentsContainer: {
     flex: 1,
     paddingHorizontal: 24,
     paddingTop: Constants.statusBarHeight + 20
   },
-  header: {
+
+  headerContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center'
   },
+
   headerText: {
     fontSize: 15,
     color: '#737380'
   },
+
   headerTextBold: {
     fontWeight: 'bold'
   },
-  title: {
+
+  mainTitle: {
     fontSize: 30,
     marginBottom: 16,
     marginTop: 48,
     color: '#13131a',
     fontWeight: 'bold'
   },
-  description: {
+
+  mainDescription: {
     fontSize: 16,
     lineHeight: 24,
     color: '#737380'
   },
-  incidentList: {
+
+  incidentsList: {
     marginTop: 32
   },
+
   incident: {
     padding: 24,
-    borderRadius: 8,
-    backgroundColor: '#fff',
+    borderRadius: 5,
+    backgroundColor: '#FFF',
     marginBottom: 16
   },
-  incidentProperty: {
-    fontSize: 14,
-    color: '#41414d',
+
+  incidentOng: {
+    fontSize: 16,
+    color: '#333',
     fontWeight: 'bold'
   },
-  incidentValue: {
-    marginTop: 8,
-    fontSize: 15,
-    marginBottom: 24,
-    color: '#737380'
+
+  incidentDescription: {
+    fontSize: 14,
+    color: '#404040',
+    marginVertical: 10,
   },
-  detailsButton: {
+
+  incidentValue: {
+    color: '#585858',
+    marginBottom: 30
+  },
+
+  incidentButton: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
     alignItems: 'center'
   },
-  detailsButtonText: {
-    color: '#e02041',
-    fontSize: 15,
-    fontWeight: 'bold'
+
+  incidentButtonText: {
+    color: '#E02041',
+    fontWeight: 'bold',
+    marginRight: 10
   }
-});
+
+})
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Detail from './pages/Detail';
-import Incidents from './pages/Incidents';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
-const AppStack = createStackNavigator();
+const AppStack = createStackNavigator()
+
+import Incidents from './pages/Incidents'
+import Details from './pages/Detail'
 
 export default function Routes() {
   return (
     <NavigationContainer>
+
       <AppStack.Navigator screenOptions={{ headerShown: false }}>
-        <AppStack.screen name="Incidents" component={Incidents} />
-        <AppStack.screen name="Detail" component={Detail} />
+        <AppStack.Screen name="Incidents" component={ Incidents }/>
+        <AppStack.Screen name="Details" component={ Details }/>
       </AppStack.Navigator>
+
     </NavigationContainer>
-  );
+  )
 }
